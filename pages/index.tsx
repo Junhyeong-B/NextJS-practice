f083d3076133456318d382bfa428c205fe0f9887
JunHyeongB <aowlr0513@gmail.com>
2022-02-02 21:58:34 +0900

    feat: Home 컴포넌트에 The Movie DB API 연결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,65 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
+type MovieData = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: false;
+  vote_average: number;
+  vote_count: number;
+};
+
+export default function Home(): JSX.Element {
+  const [movies, setMovies] = useState<MovieData[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(`/api/movies`)).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies.map((movie) => (
+        <div className="movie" key={movie.id}>
+          {/* eslint-disable-next-line */}
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
