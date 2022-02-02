123d39c11d4195bab4cf2423556ae5823bcbc226
JunHyeongB <aowlr0513@gmail.com>
2022-02-02 21:57:24 +0900

    chore: HTTP 요청 API_KEY 노출 관련 .env, next.config.js rewrites 작성

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+      },
+    ];
+  },
+};
